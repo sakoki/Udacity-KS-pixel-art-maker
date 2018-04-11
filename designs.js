7b25d3c93eefc84931934ae79d032ec141dfb974
Koki Sasagawa <koki@253-3.synergydsl.com>
2018-04-11 02:59:20 -0400

    feat: completed the javascript for pixel art maker
    Implemented JQuery to make the interactive component of this web application. makegrid() function has been created to generate a grid based off user input for grid height and width. Each cell of the block will also change to the color selected by the user.

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
+// Dynamically set the size of the table as a N by M grid
+// Choose a color
+// Click a cell in the grid to fill that cell with a chosen color.
 
-// When size is submitted by the user, call makeGrid()
 
+// Create table
+const table = $('#pixelCanvas');
+
+
+// Define makeGrid function
 function makeGrid() {
+	// Define grid rows and cols
+	let inputRows = $('#inputHeight').val();
+	let inputCols = $('#inputWeight').val();
+	// Get rid of previous grid
+	table.find('tbody').remove()
+	// Initialize grid
+	table.append('<tbody></tbody>');
+	// Build grid
+	for (let i = 0; i < inputRows; i++) {
+		table.append('<tr></tr>');
+	}
+	for (let j = 0; j < inputCols; j++) {
+		$('tr').append('<td class="blankCell"></td>');
+		// Event listener for each cell
+		$('td').on('click', function(){
+			color = $('#colorPicker').val();
+			if ($(this).hasClass('blankCell')){
+				$(this).toggleClass('blankCell');
+				$(this).css('background-color', color);
+			};
+		});
+	};
+};
 
-// Your code goes here!
-
-}
+// Call makeGrid() when submit is clicked
+$('input[type="submit"]').on('click', function (event) {
+	event.preventDefault()
+	makeGrid();
+});
